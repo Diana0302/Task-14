07339f7e2b5aa95f5ba07671ded07fbee249f24e
Diana0302 <138877250+Diana0302@users.noreply.github.com>
2024-02-24 21:11:51 +0400

    Add files via upload

---
+++ Task 14.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+class Shape {
+public:
+	  void Calculate() {
+		  std::cout << "Entadrenq hashvark anox metod e" << std::endl;
+	  }
+};
+
+class Circle : public Shape {
+public: 
+	void method1() 
+	{
+		std::cout << "jarangi mrthod" << std::endl;
+	}
+};
+
+class Rectangle : public Shape 
+{
+public: 
+	void method2() 
+	{
+		std::cout << "Aystex karox e linel dzer methody" << std::endl;
+	}
+};
+
+int main()
+{
+	Circle ob;
+	ob.Calculate();
+	ob.method1();
+	Rectangle ob1;
+	ob1.Calculate();
+	ob1.method2();
+	return 0;
+
+}
